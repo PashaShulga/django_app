b464367797c1751cac4cd8726aa6de8e20585f3d
pasha <p-shulga.95@mail.ru>
2016-04-22 20:54:03 +0300

    ver 1.0

--- untitled2/settings.py
+++ untitled2/settings.py
@@ -135,9 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
-MEDIA_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
