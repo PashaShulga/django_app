a5744001f8a258a1c6be71a91c2799e4ddd72e67
pasha <p-shulga.95@mail.ru>
2016-04-22 23:41:31 +0300

    ver 1.1

--- untitled2/settings.py
+++ untitled2/settings.py
@@ -134,10 +134,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = '/var/www/django_app/static/'
 
-STATIC_URL = 'http://185.69.153.214/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
+    '/var/www/django_app/static/',
     os.path.join(BASE_DIR, "static"),
     ]
 
