0aadb0d8f9301c71f0fed03dca88c7ce607cbf63
pasha <p-shulga.95@mail.ru>
2016-04-23 01:42:42 +0300

    ver 1.1

--- web/models.py
+++ web/models.py
@@ -9,7 +9,7 @@ class Client(models.Model):
     phone = models.CharField(max_length=20)
     website = models.CharField(max_length=520)
     user = models.ForeignKey('auth.User')
-    company_logo = models.ImageField(upload_to='/static/images/')
+    company_logo = models.ImageField(blank=True, upload_to='/var/www/django_app/static/images/')
 
     class Meta:
         db_table = 'client'
